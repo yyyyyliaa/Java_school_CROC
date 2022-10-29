91f0da823bf199d554eb6516f04e0920fd84d015
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2022-10-30 00:28:14 +0300

    finished task2

--- src/ru/croc/task2/task2.java
+++ src/ru/croc/task2/task2.java
@@ -1,5 +1,22 @@
 package ru.croc.task2;
 
-public class task2 {
-    
+public class Task2 {
+
+    public static int SumArifmeticProgression(int start, int dif, int count){
+        int res = 0;
+        int cur = start;
+        for(int i = 0; i<count; i++, cur+=dif){
+            res += cur;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int start = Integer.parseInt(args[0]);
+        int dif = Integer.parseInt(args[1]);
+        int count = Integer.parseInt(args[2]);
+
+        int res = SumArifmeticProgression(start, dif, count);
+        System.out.println("Sum = " + res);
+    }
 }
