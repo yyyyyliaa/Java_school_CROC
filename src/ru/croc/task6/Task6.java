275a396d0a7a05345103b4cb1e2ca640138dc07e
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2022-11-15 02:23:09 +0300

    fixed

--- src/ru/croc/task6/Task6.java
+++ src/ru/croc/task6/Task6.java
@@ -10,43 +10,27 @@ import java.nio.file.Paths;
 
 public class Task6{
 
-    public static enum flag{
-        NO_COMMENT,
-        SINGLE_LINE_COMMENT,
-        MULTILINE_COMMENT
-    }
-
-    public static String removeJavaComments(String source){  //flag == 0 - не комментарий
-        String res = "";                                   //flag == 1 - многострочный комментарий
-        flag f = flag.NO_COMMENT;                                        //flag == 2 - однострочный комментарий
-        for(int i = 0; i<source.length(); i++){
-            if(source.charAt(i) == '/' && source.charAt(i+1)=='*'){
-                if (f == flag.NO_COMMENT){
-                    f = flag.SINGLE_LINE_COMMENT;
-                    i++;
-                }
+    public static String removeJavaComments(String source){  
+        StringBuilder res = new StringBuilder(source);   
+                                
+        int start = res.toString().indexOf("/*");
+        
+        while(start!=-1){
+            int end = res.toString().indexOf("*/", start)+1; 
+            if(end!=-1){
+                res.replace(start, end+1, "");
             }
-            else if(source.charAt(i) == '*' && source.charAt(i+1)=='/'){
-                if(f == flag.SINGLE_LINE_COMMENT){
-                    f = flag.NO_COMMENT;
-                    i+=2;
-                }
-            }
-            else if(source.charAt(i) == '/' && source.charAt(i+1)=='/'){
-                if (f == flag.NO_COMMENT) {
-                    f = flag.MULTILINE_COMMENT;
-                    i++;
-                }
-            }
-            else if(source.charAt(i) == '\n'){
-                if (f == flag.MULTILINE_COMMENT) 
-                    f = flag.NO_COMMENT;
-            }
-            if(f == flag.NO_COMMENT){
-                res+=source.charAt(i);
-            }
+            start = res.toString().indexOf("/*");
         }
-        return res;
+        start = res.toString().indexOf("//");
+        while(start!=-1){
+            int end = res.toString().indexOf("\n", start);
+            if(end!=-1){
+                res.replace(start, end, "");
+            }
+            start = res.toString().indexOf("//");
+        }
+        return res.toString();
     }
 
     public static String readUsingFiles(String sourceFilePath) throws IOException {
@@ -62,7 +46,6 @@ public class Task6{
             System.out.println("The file does not exist or an error occurred while opening the file");
         }
         
-        // System.out.println(source);
         String result = removeJavaComments(source);
         System.out.println(result);
     }
